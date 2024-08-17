f7bc99db89af505ff3b80fa059aa6e1a15d40967
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-08-17 10:01:44 +0430

    NewGoal component added for creating new goals.

---
+++ src/components/NewGoal.tsx
@@ -0,0 +1,24 @@
+// FormEvent is a type that we use for form events objects.
+import { type FormEvent } from "react";
+
+export default function NewGoal() {
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>
+        <label htmlFor="goal">Your goal</label>
+        <input type="text" id="goal" />
+      </p>
+      <p>
+        <label htmlFor="summary">Short summary</label>
+        <input type="text" id="summary" />
+      </p>
+      <p>
+        <button>Add Goal</button>
+      </p>
+    </form>
+  );
+}
