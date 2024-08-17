428ef2c30b5d0a7eedc32e98cdc5a31d61b8e488
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-08-17 06:27:03 +0430

    handleDeleteGoal function added to App.tsx for deleting specific goal with id.

--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,14 +1,23 @@
-import { type PropsWithChildren} from "react";
-type CourseGoalProps = PropsWithChildren<{title: string}>;
+import { type PropsWithChildren } from "react";
+type CourseGoalProps = PropsWithChildren<{
+  title: string;
+  onDelete: (id: number) => void;
+  id: number;
+}>;
 
-export default function CourseGoal({ title, children }: CourseGoalProps) {
+export default function CourseGoal({
+  title,
+  id,
+  children,
+  onDelete,
+}: CourseGoalProps) {
   return (
     <article>
       <div>
         <h2>{title}</h2>
         {children}
       </div>
-      <button>Delete</button>
+      <button onClick={() => onDelete(id)}>Delete</button>
     </article>
   );
 }
